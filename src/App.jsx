c04d41b66e84b36205eb15f90f9b45cc3ef99f46
nue-os <kimsoeun0108@gmail.com>
2025-05-09 17:25:46 +0900

    Feat: 탭 컴포넌트

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useSearchParams } from 'react-router-dom'
 import './App.css'
+import Tab from './components/Tab'
 
 function App() {
-    const [count, setCount] = useState(0)
+    const tabs = [
+        { label: '기본 정보', value: 'info' },
+        { label: '조리법', value: 'recipe' },
+    ]
+    const [searchParams, setSearchParams] = useSearchParams()
+    const activeTab = searchParams.get('tab') || tabs[0].value
 
     return (
         <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+            <Tab
+                tabs={tabs}
+                activeTab={activeTab}
+                onTabSelect={(value) => setSearchParams({ tab: value })}
+            />
         </>
     )
 }
