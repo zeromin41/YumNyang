9dbbf518863b0e2021a6c23c82ccaa5856d0a4f4
Lacheln <psionict@naver.com>
2025-05-09 14:46:17 +0900

    Test: 더미데이터 단순 추가
    긴 내용의 더미데이터 단순 추가

--- src/pages/RecipeDetailPage.jsx
+++ src/pages/RecipeDetailPage.jsx
@@ -9,11 +9,16 @@ const RecipeDetailPage = () => {
         steps: [
             {
                 instruction: '단호박은 껍질을 벗기고 찐 후 으깬다.',
-                image: '/public/images/testImg.jpg',
+                image: '/images/testImg.jpg',
             },
             {
                 instruction: '닭가슴살을 익힌 후 잘게 찢는다.',
-                image: '/public/images/testImg.jpg',
+                image: '/images/testImg2.png',
+            },
+            {
+                instruction:
+                    '숙성된 갈비를 냄비에 담고 물 1컵을 부어 푹 익힙니다. 고기가 익으면 무, 당근, 대파를 넣고 국물이 자박해질 때까지 끓입니다.',
+                image: '/images/testImg.jpg',
             },
         ],
     }
