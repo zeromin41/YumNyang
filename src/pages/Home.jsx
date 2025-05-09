741f1ef51d3feaba85bb2d2fcc196a1490e8b174
seungwoo <zoozoo1302@gmail.com>
2025-05-09 17:43:42 +0900

    프로그래스바, 메뉴, 드롭다운

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nutritional from '../components/Nutritional'
+
+const Home = () => {
+  return (
+    <div>
+      <Nutritional />
+    </div>
+  )
+}
+
+export default Home
