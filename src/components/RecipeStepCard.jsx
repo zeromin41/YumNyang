aa2fa6e1006ae2dd3244bc27a4d99306d4aac6ab
Lacheln <psionict@naver.com>
2025-05-09 14:47:33 +0900

    Design: 상세페이지 카드 css 적용
    카드를 구성하는 요소들의 css적용 및 테스트 이미지와 svg파일 추가 후 적용
    (지속적인 수정 예정)

--- src/components/RecipeStepCard.jsx
+++ src/components/RecipeStepCard.jsx
@@ -3,11 +3,14 @@ import css from './RecipeStepCard.module.css'
 const RecipeStepCard = ({ number, instruction, image }) => {
     return (
         <div>
-            <div>{image && <img src={image} alt="이미지" />}</div>
-            <div>
-                <p>
+            <div className={css.recipeImg}>{image && <img src={image} alt="이미지" />}</div>
+            <div className={css.desContainer}>
+                <p className={css.description}>
                     {number}. {instruction}
                 </p>
+                <button>
+                    <img src="/images/play-03.svg" alt="" />
+                </button>
             </div>
         </div>
     )
