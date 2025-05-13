ad670cbae8fb14eb6a772433b500246edd381904
nue-os <kimsoeun0108@gmail.com>
2025-05-13 18:02:30 +0900

    Feat: 유효성 검사 메시지

---
+++ src/constants/messages.js
@@ -0,0 +1,6 @@
+export const VALIDATION_MESSAGES = {
+    ID: '아이디는 영문 소문자, 숫자를 포함한 5~20자여야 합니다.',
+    NICKNAME: '닉네임은 2~10자의 한글, 영문 또는 숫자만 가능합니다.',
+    PASSWORD: '비밀번호는 영문, 숫자, 특수문자를 포함한 8~20자여야 합니다.',
+    PASSWORD_CONFIRM: '비밀번호가 일치하지 않습니다.',
+}
