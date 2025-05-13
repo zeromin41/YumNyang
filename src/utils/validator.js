6c344323df8c2f832c0f9180921e6b02c9dfef41
nue-os <kimsoeun0108@gmail.com>
2025-05-13 18:02:08 +0900

    Feat: 아이디, 닉네임, 비밀번호, 비밀번호 확인 유효성 검사 함수

---
+++ src/utils/validator.js
@@ -0,0 +1,4 @@
+export const isValidId = (id) => /^(?=.*[a-z])(?=.*\d)[a-z\d]{5,20}$/.test(id)
+export const isValidNickname = (nickname) => /^[가-힣a-zA-Z0-9]{2,10}$/.test(nickname)
+export const isValidPassword = (pw) => /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,20}$/.test(pw)
+export const isValidPasswordConfirm = (pw, pwConfirm) => pw === pwConfirm
