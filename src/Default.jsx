e43f899ab0dc085cb1b0b8bf522f23e1ee33cf23
zeromin41 <young@YoungminGuri>
2025-05-13 17:44:40 +0900

    Feat:홈페이지 추가

--- src/Default.jsx
+++ src/Default.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import Menu from './components/Menu'
 import { Outlet } from 'react-router-dom'
-
+import Header from './components/Header'
+import styles from './Default.module.css'
 const Default = () => {
-  return (
-    <>
-        <Outlet />
-        <Menu />
-    </>
-  )
+    return (
+        <>
+            <Header />
+            <div className={styles.pageContentWrapper}>
+                <Outlet />
+            </div>
+            <Menu />
+        </>
+    )
 }
 
 export default Default
