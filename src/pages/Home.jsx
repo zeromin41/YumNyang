2c13d0df665bd5d6404b405aaca57b582138ca10
seungwoo <zoozoo1302@gmail.com>
2025-05-13 16:27:55 +0900

    feat:타이머 제작

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Nutritional from '../components/Nutritional'
+import Timer from '../components/Timer'
 
 const Home = () => {
   return (
     <div>
       <Nutritional />
+      <Timer onComplete={() => alert("완료!")}/>
     </div>
   )
 }
