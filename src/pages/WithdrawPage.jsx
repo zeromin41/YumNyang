d001294899a392f8af64c2703d411de49fcf66dc
nue-os <kimsoeun0108@gmail.com>
2025-05-14 09:36:00 +0900

    Feat: 회원탈퇴 시, 입력 유효성 검사

--- src/pages/WithdrawPage.jsx
+++ src/pages/WithdrawPage.jsx
@@ -3,6 +3,8 @@ import css from './WithdrawPage.module.css'
 import { useNavigate } from 'react-router-dom'
 import InputField from '../components/InputField'
 import Button from '../components/Button'
+import { isEmpty, isValidPasswordConfirm } from '../utils/validator'
+import { VALIDATION_MESSAGES } from '../constants/messages'
 
 const WithDrawPage = () => {
     const navigate = useNavigate()
@@ -10,11 +12,26 @@ const WithDrawPage = () => {
     const [password, setPassword] = useState('')
     const [passwordConfirm, setPasswordConfirm] = useState('')
 
+    const [passwordError, setPasswordError] = useState('')
+    const [passwordConfirmError, setPasswordConfirmError] = useState('')
+
     const handleChange = (setter) => (e) => {
         setter(e.target.value)
     }
 
+    const handleError = (isValid, errorSetter, validationMessage) => {
+        errorSetter(isValid ? '' : validationMessage)
+    }
+
     const handleWithdraw = () => {
+        // 빈 값 검증
+        handleError(!isEmpty(password), setPasswordError, VALIDATION_MESSAGES.EMPTY_PASSWORD)
+        // 비밀번호 일치 검증
+        handleError(
+            isValidPasswordConfirm(password, passwordConfirm),
+            setPasswordConfirmError,
+            VALIDATION_MESSAGES.PASSWORD_CONFIRM
+        )
         // 회원탈퇴 로직...
     }
 
@@ -25,13 +42,17 @@ const WithDrawPage = () => {
                     label="기존 비밀번호"
                     id="password"
                     value={password}
+                    type="password"
                     onChange={handleChange(setPassword)}
+                    errorMsg={passwordError}
                 />
                 <InputField
                     label="비밀번호 재입력"
                     id="passwordConfirm"
                     value={passwordConfirm}
+                    type="password"
                     onChange={handleChange(setPasswordConfirm)}
+                    errorMsg={passwordConfirmError}
                 />
             </form>
             <div className={css.btnWrapper}>
