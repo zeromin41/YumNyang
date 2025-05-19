d872d2f2a754e9bf48a4fa4874230d6dcb642caa
nue-os <kimsoeun0108@gmail.com>
2025-05-19 10:59:42 +0900

    Feat: 모달 오픈 시 브라우저 스크롤 막기

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import css from './Modal.module.css'
 
 const Modal = ({ children }) => {
+    useEffect(() => {
+        const originalStyle = window.getComputedStyle(document.body).overflow
+        document.body.style.overflow = 'hidden' // 스크롤 막기
+
+        return () => {
+            document.body.style.overflow = originalStyle // 모달이 닫히면 복구
+        }
+    }, [])
+
     return (
         <div className={css.modal}>
             <div className={css.modalCon}>{children}</div>
