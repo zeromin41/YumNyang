60b0e716074f0b7d7b8dbbadb9a70bd794347558
Lacheln <psionict@naver.com>
2025-05-19 13:22:13 +0900

    Refactor: 성분 나트륨, 칼륨 추가

--- src/components/Nutritional.jsx
+++ src/components/Nutritional.jsx
@@ -3,7 +3,17 @@ import style from './Nutritional.module.css'
 import chartImg from '../assets/chart.svg'
 const Nutritional = ({ recipeData }) => {
     // 영양소 명, 이전 변수명 : title
-    const ingredientName = ['칼슘', '탄수화물', '지방', '식이섬유', '수분', '인', '단백질']
+    const ingredientName = [
+        '칼슘',
+        '탄수화물',
+        '지방',
+        '식이섬유',
+        '수분',
+        '인',
+        '단백질',
+        '나트륨',
+        '칼륨',
+    ]
 
     //영양소 양, 이전 변수명 : rating,  undefined, null일 경우 0
     const amount = [
@@ -14,6 +24,8 @@ const Nutritional = ({ recipeData }) => {
         recipeData.recipe.NUTRITIONAL_INFO_MOISTURE_PERCENT ?? 0,
         recipeData.recipe.NUTRITIONAL_INFO_PHOSPHORUS_G ?? 0,
         recipeData.recipe.NUTRITIONAL_INFO_PROTEIN_G ?? 0,
+        recipeData.recipe.NUTRITIONAL_INFO_NACL_G ?? 0,
+        recipeData.recipe.NUTRITIONAL_INFO_PTSS_G ?? 0,
     ]
 
     return (
