152cf84f674f9c47cc344f36fa639d6e57b824cc
Lacheln <psionict@naver.com>
2025-05-20 13:16:22 +0900

    Feat: default 이미지 추가

--- src/components/RecipeStepCard.jsx
+++ src/components/RecipeStepCard.jsx
@@ -2,10 +2,20 @@ import React from 'react'
 import css from './RecipeStepCard.module.css'
 import TTSComponent from './TTSComponent'
 import playImg from '../assets/volume-06.svg'
+import defaultImg from '../assets/defaultImg.svg'
 const RecipeStepCard = ({ number, instruction, image, btnkey }) => {
     return (
         <div className={css.mainContainer}>
-            <div className={css.recipeImgWrap}>{image && <img src={image} alt="이미지" />}</div>
+            <div className={css.recipeImgWrap}>
+                <img
+                    src={image || defaultImg}
+                    alt="이미지"
+                    onError={(e) => {
+                        e.target.onerror = null // 무한 루프 방지
+                        e.target.src = defaultImg
+                    }}
+                />
+            </div>
             <div className={css.desContainer}>
                 <span className={css.des}>
                     {number}. {instruction}
